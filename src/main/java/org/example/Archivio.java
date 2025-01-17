8d6bdfaf04b24eca6146ee89d8af4d6f4c519f0a
marty957 <fantuzzimartina@yahoo.it>
2025-01-17 19:08:07 +0100

    secondo commit

--- src/main/java/org/example/Archivio.java
+++ src/main/java/org/example/Archivio.java
@@ -4,116 +4,148 @@ package org.example;
 import com.github.javafaker.Faker;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Archivio {
 
-    public static Faker fake=new Faker(new Locale("it"));
-    public static Scanner sc= new Scanner(System.in);
+    public static Faker fake = new Faker(new Locale("it"));
+    public static Scanner sc = new Scanner(System.in);
 
-   private static Map<Pubblicazioni, Long> listaPubblicazioni=new HashMap<Pubblicazioni, Long>();
+    private static Map<Long,Pubblicazioni> listaPubblicazioni = new HashMap<Long,Pubblicazioni>();
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IsbnInesitente {
 
-        creazioneArchivio();
-        creazioneArchivio().forEach(((pubblicazioni, i) ->
+        creazioneArchivio().forEach(((i, pubblicazioni) ->
                 System.out.println(pubblicazioni.toString())));
 
         aggiungiLibri(21);
+        aggiungiRivista(22,"la stagione calcistica",2);
         ricercaPub(2);
+        eliminaPub(21);
+        ricercaAnnoPub(1950);
+
+
+
 
 
     }
 
-   public static   Map<Pubblicazioni,Long> creazioneArchivio() {
-       for (int i = 0; i < 10; i++) {
+    public static Map<Long,Pubblicazioni> creazioneArchivio() {
+        for (int i = 0; i < 10; i++) {
+            Pubblicazioni p = new Libri(i + 1, fake.book().title(), fake.number()
+                    .numberBetween(1890, 2023), fake.number().numberBetween(40, 3500), fake.book()
+                    .author(), fake.book().genre());
+            listaPubblicazioni.put(p.ISBN,p);
+        }
 
-           Pubblicazioni p = new Libri(i+1, fake.book().title(), fake.number()
-                   .numberBetween(1890, 2023), fake.number().numberBetween(40, 3500), fake.book()
-                   .author(), fake.book().genre());
-           listaPubblicazioni.put(p, p.ISBN);
-
-       }
-       Pubblicazioni r1 = new Riviste(11, "ELLE DECOR", fake.number().numberBetween(1920, 2020),
-               fake.number().numberBetween(20, 150), Periodicita.SETTIMANALE);
-       Pubblicazioni r2 = new Riviste(12, "MARIE CLAIRE", fake.number().numberBetween(1920, 2020),
-               fake.number().numberBetween(20, 150), Periodicita.MENSILE);
-       Pubblicazioni r3 = new Riviste(13, "Focus italia", fake.number().numberBetween(1920, 2020),
-               fake.number().numberBetween(20, 150), Periodicita.SETTIMANALE);
-       Pubblicazioni r4 = new Riviste(14, "ELLE", fake.number().numberBetween(1920, 2020),
-               fake.number().numberBetween(20, 150), Periodicita.MENSILE);
-       Pubblicazioni r5 = new Riviste(15, "Icon", fake.number().numberBetween(1920, 2020),
-               fake.number().numberBetween(20, 150), Periodicita.MENSILE);
-       Pubblicazioni r6 = new Riviste(16, "Forbes", fake.number().numberBetween(1920, 2020),
-               fake.number().numberBetween(20, 150), Periodicita.SETTIMANALE);
-       Pubblicazioni r7 = new Riviste(17, "Chi", fake.number().numberBetween(1920, 2020),
-               fake.number().numberBetween(20, 150), Periodicita.SETTIMANALE);
-       Pubblicazioni r8 = new Riviste(18, "Guinness World Records", fake.number().numberBetween(1920, 2020),
-               fake.number().numberBetween(20, 150), Periodicita.ANNUALE);
-       Pubblicazioni r9 = new Riviste(19, "Scoperte scentifiche dell'anno", fake.number().numberBetween(1920, 2020),
-               fake.number().numberBetween(20, 150), Periodicita.ANNUALE);
-       Pubblicazioni r10 = new Riviste(20, "National Geographic Magazine", fake.number().numberBetween(1920, 2020),
-               fake.number().numberBetween(20, 150), Periodicita.MENSILE);
-       listaPubblicazioni.put(r1, r1.ISBN);
-       listaPubblicazioni.put(r2, r2.ISBN);
-       listaPubblicazioni.put(r3, r3.ISBN);
-       listaPubblicazioni.put(r4, r4.ISBN);
-       listaPubblicazioni.put(r5, r5.ISBN);
-       listaPubblicazioni.put(r6, r6.ISBN);
-       listaPubblicazioni.put(r7, r7.ISBN);
-       listaPubblicazioni.put(r8, r8.ISBN);
-       listaPubblicazioni.put(r9, r9.ISBN);
-       listaPubblicazioni.put(r10, r10.ISBN);
-       return listaPubblicazioni;
-   }
+        Pubblicazioni r1 = new Riviste(11, "ELLE DECOR", fake.number().numberBetween(1920, 2020),
+                fake.number().numberBetween(20, 150), Periodicita.SETTIMANALE);
+        Pubblicazioni r2 = new Riviste(12, "MARIE CLAIRE", fake.number().numberBetween(1920, 2020),
+                fake.number().numberBetween(20, 150), Periodicita.MENSILE);
+        Pubblicazioni r3 = new Riviste(13, "Focus italia", 1950,
+                fake.number().numberBetween(20, 150), Periodicita.SETTIMANALE);
+        Pubblicazioni r4 = new Riviste(14, "ELLE", fake.number().numberBetween(1920, 2020),
+                fake.number().numberBetween(20, 150), Periodicita.MENSILE);
+        Pubblicazioni r5 = new Riviste(15, "Icon", fake.number().numberBetween(1920, 2020),
+                fake.number().numberBetween(20, 150), Periodicita.MENSILE);
+        Pubblicazioni r6 = new Riviste(16, "Forbes", fake.number().numberBetween(1920, 2020),
+                fake.number().numberBetween(20, 150), Periodicita.SETTIMANALE);
+        Pubblicazioni r7 = new Riviste(17, "Chi", fake.number().numberBetween(1920, 2020),
+                fake.number().numberBetween(20, 150), Periodicita.SETTIMANALE);
+        Pubblicazioni r8 = new Riviste(18, "Guinness World Records", fake.number().numberBetween(1920, 2020),
+                fake.number().numberBetween(20, 150), Periodicita.ANNUALE);
+        Pubblicazioni r9 = new Riviste(19, "Scoperte scentifiche dell'anno", fake.number().numberBetween(1920, 2020),
+                fake.number().numberBetween(20, 150), Periodicita.ANNUALE);
+        Pubblicazioni r10 = new Riviste(20, "National Geographic Magazine", fake.number().numberBetween(1920, 2020),
+                fake.number().numberBetween(20, 150), Periodicita.MENSILE);
+        listaPubblicazioni.put( r1.ISBN,r1);
+        listaPubblicazioni.put(r2.ISBN,r1);
+        listaPubblicazioni.put( r3.ISBN,r3);
+        listaPubblicazioni.put(r4.ISBN,r4);
+        listaPubblicazioni.put(r5.ISBN,r5);
+        listaPubblicazioni.put(r6.ISBN,r6);
+        listaPubblicazioni.put(r7.ISBN,r7);
+        listaPubblicazioni.put(r8.ISBN,r8);
+        listaPubblicazioni.put(r9.ISBN,r9);
+        listaPubblicazioni.put(r10.ISBN,r10);
 
 
-public static void aggiungiLibri(long i){
+        return listaPubblicazioni;
+    }
 
-Libri l=new Libri(i,fake.book().title(),fake.number().numberBetween(1950,2022),fake.number()
-        .numberBetween(49,5000),fake.book().author(),fake.book().genre());
-listaPubblicazioni.put(l,i);
-    System.out.println("LIBRO AGGIUNTO: " + l);
 
-}
-    public static void aggiungiRivista(long i,String titolo, int p){
+    public static void aggiungiLibri(long i) {
 
-        if(p==1){
-        Riviste r =new Riviste(i,titolo,fake.number().numberBetween(1950,2022),fake.number()
-                .numberBetween(49,5000),Periodicita.SETTIMANALE);
-        listaPubblicazioni.put(r,i);
-        System.out.println("RIVISTA AGGIUNTA: " + r);
-        } else if (p==2) {
-            Riviste r =new Riviste(i,titolo,fake.number().numberBetween(1950,2022),fake.number()
-                    .numberBetween(49,5000),Periodicita.MENSILE);
-            listaPubblicazioni.put(r,i);
+        Libri l = new Libri(i, fake.book().title(), fake.number().numberBetween(1950, 2022), fake.number()
+                .numberBetween(49, 5000), fake.book().author(), fake.book().genre());
+        listaPubblicazioni.put( i,l);
+        System.out.println("LIBRO AGGIUNTO: " + l);
+
+    }
+
+    public static void aggiungiRivista(long i, String titolo, int p) {
+
+        if (p == 1) {
+            Riviste r = new Riviste(i, titolo, fake.number().numberBetween(1950, 2022), fake.number()
+                    .numberBetween(49, 5000), Periodicita.SETTIMANALE);
+            listaPubblicazioni.put( i,r);
             System.out.println("RIVISTA AGGIUNTA: " + r);
-        } else if (p==3) {
-            Riviste r =new Riviste(i,titolo,fake.number().numberBetween(1950,2022),fake.number()
-                    .numberBetween(49,5000),Periodicita.ANNUALE);
-            listaPubblicazioni.put(r,i);
+        } else if (p == 2) {
+            Riviste r = new Riviste(i, titolo, fake.number().numberBetween(1950, 2022), fake.number()
+                    .numberBetween(49, 5000), Periodicita.MENSILE);
+            listaPubblicazioni.put(i,r);
+            System.out.println("RIVISTA AGGIUNTA: " + r);
+        } else if (p == 3) {
+            Riviste r = new Riviste(i, titolo, fake.number().numberBetween(1950, 2022), fake.number()
+                    .numberBetween(49, 5000), Periodicita.ANNUALE);
+            listaPubblicazioni.put(i,r);
             System.out.println("RIVISTA AGGIUNTA: " + r);
 
         }
 
     }
 
+    public static void ricercaPub(long i) throws IsbnInesitente {
 
-    public static void ricercaPub(long i) {
-        Pubblicazioni pub = null;
-        for (Pubblicazioni p : listaPubblicazioni.keySet()) {
-            if (i == p.ISBN) {
-                pub = p;
-                break;
-            }
+        if(listaPubblicazioni.containsKey(i)){
+            System.out.println("PUBBLICAZIONE RICERCATA: "+ listaPubblicazioni.get(i));
         }
-            if (pub != null) {
-                System.out.println(pub);
-            } else {
-                System.out.println("pubblicazione inesistente");
-            }
-
+        else {
+            throw new IsbnInesitente("Codice ISBN non valido. Pubblicazione inesistente");
+        }
         }
 
+        public static void eliminaPub(long i){
+            System.out.println( "pubblicazione eliminata: " + listaPubblicazioni.get(i));
+        listaPubblicazioni.remove(i);
+
+        }
+
+        public static List<Pubblicazioni> ricercaAnnoPub(long anno){
+
+        List<Pubblicazioni> ricercaPerAnno=new ArrayList<Pubblicazioni>(listaPubblicazioni.values());
+        ricercaPerAnno.stream().filter(pub -> anno== pub.annoPubblicazione )
+                .forEach(ele-> System.out.println("Pubblicazioni ricercate: "+ele));
+
+        return ricercaPerAnno;
+
+        };
+    public static List<Libri> ricercaAutore(String autore){
+
+        List<Libri> ricercaPerAnno=new ArrayList<Libri>(listaPubblicazioni.values(<Libri>));
+        ricercaPerAnno.stream().filter(pub -> autore.equals(pub.get) )
+                .forEach(ele-> System.out.println("Pubblicazioni ricercate: "+ele));
+
+        return ricercaPerAnno;
+
+    };
 
     }
+
+
+
+
+
+
+
---
+++ src/main/java/org/example/IsbnInesitente.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class IsbnInesitente extends Exception {
+    public IsbnInesitente(String message) {
+        super(message);
+    }
+}
